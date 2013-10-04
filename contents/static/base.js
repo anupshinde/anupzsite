2da53545023e2f83788e529e2a9e22526440573c
Anup Shinde <anshinde@BMC-GNNS7X1.adprod.bmc.com>
2013-10-04 18:17:30 +0530

    Fixing FB likebox

--- contents/static/base.js
+++ contents/static/base.js
@@ -6,3 +6,12 @@ function getParameterByName(name) {
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+/* -- sofbs --*/
+(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&&appId=774977289184415";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));
+/* -- eofbs --*/
